2c627091b6fa6de7ae5e6edcd6a5c28e54b78470
george4me <118745391+george4me@users.noreply.github.com>
2023-07-09 20:33:47 +0200

    static files 2

--- project/settings.py
+++ project/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 if DEBUG: 
     STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     STATIC_ROOT = BASE_DIR / "static"
 
