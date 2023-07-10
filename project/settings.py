bdfd2d7f8a822bd6c5e90e9339d57a8206349276
george4me <118745391+george4me@users.noreply.github.com>
2023-07-10 21:18:56 +0200

    static dir

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env()
 SECRET_KEY = config("SK")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,12 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-"""if DEBUG: 
+"""
+if DEBUG: 
     STATICFILES_DIRS = [BASE_DIR / "static"]
    
 else:
     #STATIC_ROOT = BASE_DIR / "static"
 """
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
