e4000aeb660bdfabe7b6cbca290aa77abd8b08a9
george4me <118745391+george4me@users.noreply.github.com>
2023-07-10 21:13:45 +0200

    update 10 july 9pm

--- project/settings.py
+++ project/settings.py
@@ -127,13 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG: 
+"""if DEBUG: 
     STATICFILES_DIRS = [BASE_DIR / "static"]
    
 else:
     #STATIC_ROOT = BASE_DIR / "static"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+"""
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
